d1b84fc8ca0079dff9ba53b40f208b0bad50bc16
Festus Daramola <123788749+afdaraola@users.noreply.github.com>
2023-03-05 17:34:40 -0500

    Create list-ofvalue-divisibleby-3.sql

---
+++ list-ofvalue-divisibleby-3.sql
@@ -0,0 +1,23 @@
+--get the list of values divisible by 3 that are less than 100
+
+
+
+--method 1
+
+select listagg(r,',') within group ( order by r desc ) list from (
+                                                                select rownum as r
+                                                                from USER_TABLES
+                                                                where rownum <= 100
+                                                            ) where mod(r,3) = 0;
+                                                            
+                                                            
+                                                            
+--method 2
+
+select listagg(r,'|') within group ( order by r desc ) as list from  (
+select ROWNUM as r  from DUAL connect by level <=100
+) where mod(r,3)=0;
+
+--method 3
+select listagg(r,',') within group ( order by r desc )
+from (select ROWNUM*3 as r from DUAL  connect by level <= 100/3);
